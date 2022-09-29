a5720870e24c22cea6517c56520f9b0b6dea6649
harsha070795 <harsha.nayaka@dedikh.com>
2022-09-29 15:39:10 +0530

    Added/Changes files in root directory : - .ebignore - .npmrc - package.json - Procfile - database.js - plugins.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,19 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'dedikh_backend'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'root'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "dedikh_backend"),
+          user: env("DATABASE_USERNAME", "postgres"),
+          password: env("DATABASE_PASSWORD", "root"),
+          ssl: env.bool("DATABASE_SSL", false),
+        },
+      },
+    };
+  } else {
+    return {};
+  }
+};
