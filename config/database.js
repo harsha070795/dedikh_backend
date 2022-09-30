71af5a20f03e1294f7a3ff4e293756ce3180d688
harsha070795 <harsha.nayaka@dedikh.com>
2022-09-30 17:13:35 +0530

    Changes in database.js and server.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("AWS_RDS_DATABASE_HOST"),
+      host: process.env("AWS_RDS_DATABASE_HOST"),
       port: env.int("AWS_RDS_DATABASE_PORT"),
-      database: env("AWS_RDS_DATABASE_NAME"),
-      user: env("AWS_RDS_DATABASE_USERNAME"),
-      password: env("AWS_RDS_DATABASE_PASSWORD"),
+      database: process.env("AWS_RDS_DATABASE_NAME"),
+      user: process.env("AWS_RDS_DATABASE_USERNAME"),
+      password: process.env("AWS_RDS_DATABASE_PASSWORD"),
       ssl: {
         rejectUnauthorized: false,
       },
