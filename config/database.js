ca5185e9c0dfa5ae7a178126f66ae768c98b4723
harsha070795 <harsha.nayaka@dedikh.com>
2022-09-30 16:21:05 +0530

    1.Added .ebextension file in root directory        2. Changes in procfile and database.js file

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    return {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: env("RDS_DATABASE_HOST"),
-          port: env.int("RDS_DATABASE_PORT"),
-          database: env("RDS_DATABASE_NAME"),
-          user: env("RDS_DATABASE_USERNAME"),
-          password: env("RDS_DATABASE_PASSWORD"),
-          ssl: env.bool("DATABASE_SSL", false),
-        },
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("AWS_RDS_DATABASE_HOST"),
+      port: env.int("AWS_RDS_DATABASE_PORT"),
+      database: env("AWS_RDS_DATABASE_NAME"),
+      user: env("AWS_RDS_DATABASE_USERNAME"),
+      password: env("AWS_RDS_DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: false,
       },
-    };
-  } else {
-    return {};
-  }
-};
+    },
+  },
+});
