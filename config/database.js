64a15ca437937014b9fa042b23fa315978a44ee5
harsha070795 <harsha.nayaka@dedikh.com>
2022-09-30 17:25:44 +0530

    Added dotenve package.

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
+require('dotenv').config
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: process.env("AWS_RDS_DATABASE_HOST"),
-      port: env.int("AWS_RDS_DATABASE_PORT"),
-      database: process.env("AWS_RDS_DATABASE_NAME"),
-      user: process.env("AWS_RDS_DATABASE_USERNAME"),
-      password: process.env("AWS_RDS_DATABASE_PASSWORD"),
+      host: process.env.AWS_RDS_DATABASE_HOST,
+      database: process.env.AWS_RDS_DATABASE_NAME,
+      user: process.env.AWS_RDS_DATABASE_USERNAME,
+      password: process.env.AWS_RDS_DATABASE_PASSWORD,
+      port: 5432,
       ssl: {
         rejectUnauthorized: false,
       },
