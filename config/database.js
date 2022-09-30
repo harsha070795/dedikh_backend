8b831bc4ee0716dbb8c3aca19e0c195acf807be5
harsha070795 <harsha.nayaka@dedikh.com>
2022-09-30 10:30:57 +0530

    Changes in username,host,db name in database.js file

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => {
       connection: {
         client: "postgres",
         connection: {
-          host: env("DATABASE_HOST", "127.0.0.1"),
+          host: env("DATABASE_HOST", "awseb-e-aumkkwui9b-stack-awsebrdsdatabase-ppel99xnbrqf.ct7bv7v3kxwt.ap-south-1.rds.amazonaws.com"),
           port: env.int("DATABASE_PORT", 5432),
-          database: env("DATABASE_NAME", "dedikh_backend"),
-          user: env("DATABASE_USERNAME", "postgres"),
-          password: env("DATABASE_PASSWORD", "root"),
+          database: env("DATABASE_NAME", "ebdb"),
+          user: env("DATABASE_USERNAME", "dipansharma"),
+          password: env("DATABASE_PASSWORD", "dt-20082101"),
           ssl: env.bool("DATABASE_SSL", false),
         },
       },
