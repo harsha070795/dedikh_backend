7c9ce2d3dbaa7149210c5e3bb8b8ce77c22e972d
harsha070795 <harsha.nayaka@dedikh.com>
2022-10-07 12:34:11 +0530

    edited server.js file

--- config/server.js
+++ config/server.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
   host: process.env.APP_HOST || '0.0.0.0',
   port: env.int('PORT', 1337),
   app: {
-    keys: process.env.array('APP_KEYS'),
+    keys: process.env.array.APP_KEYS,
   },
 });
