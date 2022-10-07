e6ca9b7f622d5f650e71614298c47aea5d364fd2
harsha070795 <harsha.nayaka@dedikh.com>
2022-10-07 13:09:18 +0530

    Changes in server.js keys

--- config/server.js
+++ config/server.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
   host: process.env.APP_HOST || '0.0.0.0',
   port: env.int('PORT', 1337),
   app: {
-    keys: process.env.array.APP_KEYS,
+    keys: env.array('APP_KEYS',['myKey1','myKey2','myKey3','myKey4']),
   },
 });
