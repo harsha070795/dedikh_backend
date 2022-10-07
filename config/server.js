823f7cf416d26553b5f19e80e8c43d35bbe6259a
harsha070795 <harsha.nayaka@dedikh.com>
2022-10-07 18:02:46 +0530

    Added public url in server.js file

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ require('dotenv').config
 module.exports = ({ env }) => ({
   host: process.env.APP_HOST || '0.0.0.0',
   port: env.int('PORT', 1337),
+  url: process.env.PUBLIC_URL || 'http://dedikhbackenddev-env-1.eba-qujghbp3.ap-south-1.elasticbeanstalk.com',
   app: {
     keys: env.array('APP_KEYS',['myKey1','myKey2','myKey3','myKey4']),
   },
